05f4ebe702c5bdbc12577d5a37399a5852d9d457
Nans THOMAS <nansthomas@MacBook-de-Nans.local>
2016-02-03 16:32:19 +0100

    Lighter Version 🐦

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,2 @@
 var body        = document.querySelector('body'),
     screen      = document.querySelector('.screen');
-
-setInterval(function(){
-
-    if (body.classList == 'viewing-page-1') {
-        screen.src = 'assets/img/screen1.jpg';
-    } else if (body.classList == 'viewing-page-2') {
-        screen.src = 'assets/img/screen2.jpg';
-    } else if (body.classList == 'viewing-page-3') {
-        screen.src = 'assets/img/screen3.jpg';
-    }
-
-}, 100);
