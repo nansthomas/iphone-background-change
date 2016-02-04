7367ba90bff0baeeae7fbaa0507450b8556ffddf
Nans THOMAS <nansthomas@MacBook-de-Nans.local>
2016-02-04 16:52:34 +0100

    Beautiful Hand 👌🏽

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
 var body        = document.querySelector('body'),
     screen      = document.querySelector('.screen'),
-    clouds       = document.querySelector('#clouds');
+    clouds       = document.querySelector('#clouds'),
+    target      = document.querySelector('#Page-1');
